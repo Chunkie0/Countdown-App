b84d77e42e85fb58ee83a25caf5be2de4fd08216
Davis <davisdambis1@gmail.com>
2021-06-28 15:33:03 +0300

    added small modification

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 userInput = input("Enter your goal with a deadline seperated by colon example: learn python:20.08.2021\n")
 while ":" not in userInput:
@@ -9,8 +9,8 @@ goal = inputList[0]
 deadline = inputList[1]
 
 
-deadlineDate = datetime.datetime.strptime(deadline, "%d.%m.%Y")
-todayDate = datetime.datetime.today()
+deadlineDate = datetime.strptime(deadline, "%d.%m.%Y")
+todayDate = datetime.today()
 daysRemaining = deadlineDate - todayDate
 hoursRemaining = int(daysRemaining.total_seconds() / 60 / 60)
 print(f"Dear user! Time remaining to {goal}: is {daysRemaining} days and in hours: {hoursRemaining}h.")
