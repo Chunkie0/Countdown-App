b81b0e64f90b251e59b93ba6daadcff5f9bf28c2
Davis <davisdambis1@gmail.com>
2021-06-28 15:23:54 +0300

    Added code to check how much time remaining for a goal

---
+++ index.py
@@ -0,0 +1,16 @@
+import datetime
+
+userInput = input("Enter your goal with a deadline seperated by colon example: learn python:20.08.2021\n")
+while ":" not in userInput:
+    userInput = input("Please seperate by colon. Try again:\n")
+inputList = userInput.split(":")
+
+goal = inputList[0]
+deadline = inputList[1]
+
+
+deadlineDate = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+todayDate = datetime.datetime.today()
+daysRemaining = deadlineDate - todayDate
+hoursRemaining = int(daysRemaining.total_seconds() / 60 / 60)
+print(f"Dear user! Time remaining to {goal}: is {daysRemaining} days and in hours: {hoursRemaining}h.")
